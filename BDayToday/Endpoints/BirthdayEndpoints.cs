7c69dae6ed7fe878d8814f89e91f40ce6684a6b4
Anastasiya Abramova <65863242+abramana@users.noreply.github.com>
2024-03-19 14:19:06 +0100

    Added create, update, delete birthday endpoints (#4)

--- BDayToday/Endpoints/BirthdayEndpoints.cs
+++ BDayToday/Endpoints/BirthdayEndpoints.cs
@@ -1,23 +1,57 @@
 using BDayToday.Endpoints.Internal;
+using BDayToday.Models;
 using BDayToday.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BDayToday.Endpoints;
 
 public class BirthdayEndpoints : IEndpoints
 {
+    private const string BaseRoute = "birthday";
+
     public static void DefineEndpoints(IEndpointRouteBuilder app)
     {
-        app.MapGet("birthday", GetAllBirthdaysAsync);
+        app.MapGet(BaseRoute, GetAllBirthdaysAsync);
+        app.MapPost(BaseRoute, CreateBirthdayAsync);
+        app.MapPut(BaseRoute, UpdateBirthdayAsync);
+        app.MapDelete(BaseRoute, DeleteBirthdayAsync);
     }
 
     public static void AddServices(IServiceCollection services, IConfiguration configuration)
     {
     }
 
-    internal static async Task<IResult> GetAllBirthdaysAsync(
-        IBirthdayService birthdayService, string? searchTerm)
+    private static async Task<IResult> GetAllBirthdaysAsync(
+        IBirthdayService birthdayService)
     {
         var birthdays = await birthdayService.GetAllAsync();
         return Results.Ok(birthdays);
+    }
+
+    private static async Task<IResult> CreateBirthdayAsync(
+        [FromBody] Birthday birthday, [FromServices] IBirthdayService birthdayService)
+    {
+        var created = await birthdayService.CreateAsync(birthday);
+        return created
+            ? Results.Created($"/{BaseRoute}/{birthday.Name}", birthday)
+            : Results.BadRequest("A person with this name already exists");
+    }
+
+    private static async Task<IResult> UpdateBirthdayAsync(
+        [FromBody] Birthday birthday, [FromServices] IBirthdayService birthdayService)
+    {
+        var updated = await birthdayService.UpdateAsync(birthday);
+        return updated
+            ? Results.Ok(birthday)
+            : Results.NotFound();
+    }
+
+    private static async Task<IResult> DeleteBirthdayAsync(
+        [FromBody] Birthday birthday, [FromServices] IBirthdayService birthdayService)
+    {
+        var deleted = await birthdayService.DeleteAsync(birthday);
+        return deleted
+            ? Results.Ok(birthday)
+            : Results.NotFound();
     }
 }
--- BDayToday/Services/IBirthdayService.cs
+++ BDayToday/Services/IBirthdayService.cs
@@ -5,4 +5,7 @@ namespace BDayToday.Services;
 public interface IBirthdayService
 {
     public Task<IEnumerable<Birthday>> GetAllAsync();
+    public Task<bool> CreateAsync(Birthday birthday);
+    public Task<bool> UpdateAsync(Birthday birthday);
+    public Task<bool> DeleteAsync(Birthday birthday);
 }
